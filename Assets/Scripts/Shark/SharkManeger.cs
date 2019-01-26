480dd86430bc0056beff7812fc95be209fa7dec3
MizukamiTakahiro <tadanosimezi@gmail.com>
2019-01-26 11:16:42 +0900

    UIとサメ関係のスクリプト追加

---
+++ Assets/Scripts/Shark/SharkManeger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkManeger : MonoBehaviour
+{
+	//サメ出現までの時間最大値
+	[SerializeField] float timerMax = 0;
+
+	//サメ出現までの時間
+	float timer;
+
+	//タイマー減少を行うか
+	bool timerFlag = true;
+
+	private void Start()
+	{
+		timer = timerMax;
+	}
+
+	private void Update()
+	{
+		if(timerFlag)
+		{
+			timer -= Time.deltaTime;
+			if(timer <= 0)
+			{
+				//サメ出現までの時間が0になった時に1度だけ行う処理
+
+				timerFlag = false;
+			}
+		}
+	}
+
+	/// <summary>
+	/// サメ出現までの時間を取得
+	/// </summary>
+	public float GetTime()
+	{
+		return timer;
+	}
+
+	/// <summary>
+	/// サメ出現までの時間を割合で取得(0~100)
+	/// </summary>
+	public float GetTimeProportion()
+	{
+		return (timer / timerMax) * 100;
+	}
+
+}

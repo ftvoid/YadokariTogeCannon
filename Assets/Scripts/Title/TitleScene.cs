3af29d30fa4470bb48d3c4a410d3e4c2ffd35854
ftvoid <gensaicuttlefish@gmail.com>
2019-01-27 08:57:42 +0900

    タイトルシーン追加。 タイトルからゲーム開始できるようにした。

---
+++ Assets/Scripts/Title/TitleScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// タイトルシーン
+/// </summary>
+public class TitleScene : MonoBehaviour
+{
+    private void Update()
+    {
+        if ( Input.GetButtonDown("Submit") )
+        {
+            SceneChanger.Load("mainScene");
+        }
+    }
+}

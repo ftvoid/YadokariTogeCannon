3c39e8268241029e6b181c1b98df072edd06ed3e
ftvoid <gensaicuttlefish@gmail.com>
2019-01-27 13:19:50 +0900

    リザルトシーン修正。 タイトルロゴ差し替え。

--- Assets/Scripts/Result/ResultScene.cs
+++ Assets/Scripts/Result/ResultScene.cs
@@ -11,16 +11,23 @@ public class ResultScene : MonoBehaviour
     [SerializeField]
     private Text _message;
 
+    [Header("オカヤドカリ 到達Lv"), SerializeField]
+    private int _rankBLv = 4;
+
+    [Header("コモンヤドカリ 到達Lv"), SerializeField]
+    private int _rankALv = 8;
+
+    [Header("タラバガニ 到達Lv"), SerializeField]
+    private int _rankSLv = 12;
+
     int _growthLv;
 
     private void Start()
     {
         SoundManager.Instance.PlayBGM("Clear");
 
-        Debug.Log("SceneChanger.Instance.SceneParams = " + SceneChanger.Instance.SceneParams);
-
         object growthLv = null;
-        SceneChanger.Instance.SceneParams?.TryGetValue("growthLv", out growthLv);
+        SceneChanger.Instance?.SceneParams?.TryGetValue("growthLv", out growthLv);
 
         if ( growthLv != null && growthLv is int )
         {
@@ -34,17 +41,17 @@ public class ResultScene : MonoBehaviour
         string yadokariType;
         int size;
 
-        if ( _growthLv < 4 )
+        if ( _growthLv < _rankBLv )
         {
             yadokariType = "ホンヤドカリ";
             size = 1;
         }
-        else if ( _growthLv < 8 )
+        else if ( _growthLv < _rankALv )
         {
             yadokariType = "オカヤドカリ";
             size = 5;
         }
-        else if ( _growthLv < 12 )
+        else if ( _growthLv < _rankSLv )
         {
             yadokariType = "コモンヤドカリ";
             size = 20;

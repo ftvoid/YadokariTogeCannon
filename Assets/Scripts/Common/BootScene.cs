d1a296db6a27dac19979113f223d40f0423f4bc4
ftvoid <gensaicuttlefish@gmail.com>
2019-01-27 12:51:14 +0900

    ブート用シーン追加。

---
+++ Assets/Scripts/Common/BootScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BootScene : MonoBehaviour
+{
+    private void Start()
+    {
+        SceneChanger.Load("TitleScene");
+    }
+}

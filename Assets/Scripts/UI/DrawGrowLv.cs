c5db5928df6559fc5808eab6f63f4f1084ed8f0d
gaku <yukiyuki0713@hotmail.com>
2019-01-27 14:37:52 +0900

    育成レベルをUI表示できるように変更

---
+++ Assets/Scripts/UI/DrawGrowLv.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawGrowLv : MonoBehaviour
+{
+    Text text;
+    private void Start()
+    {
+        text = GetComponent<Text>();
+    }
+    
+    private void Update()
+    {
+        text.text = "Lv."+StateManager.Instance.GetGrowthLv();
+    }
+}
